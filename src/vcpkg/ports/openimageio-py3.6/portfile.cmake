48f60e58756313f2c4dfcca5012534bdf4c090c2
Fredrik Averpil <fredrik@averpil.com>
2019-01-28 12:52:59 +0100

    Use hash as comment

--- src/vcpkg/ports/openimageio-py3.6/portfile.cmake
+++ src/vcpkg/ports/openimageio-py3.6/portfile.cmake
@@ -1,4 +1,4 @@
-// https://github.com/Microsoft/vcpkg/blob/9fe14bc18ec4b8f12238cd8790c44e42325f8e52/ports/openimageio/portfile.cmake
+# https://github.com/Microsoft/vcpkg/blob/9fe14bc18ec4b8f12238cd8790c44e42325f8e52/ports/openimageio/portfile.cmake
 
 include(vcpkg_common_functions)
 
