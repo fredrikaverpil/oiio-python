3a513c0d8297c0ef72e40f3f115960c81354eedb
Sidney Guenther <mail@sidney-guenther.de>
2019-01-31 11:40:20 +0100

    added if statement to the openimageio portfile to check the current OS

--- src/vcpkg/ports/openimageio-py3.6/portfile.cmake
+++ src/vcpkg/ports/openimageio-py3.6/portfile.cmake
@@ -32,11 +32,14 @@ endif()
 
 set(ENV{OIIO_PYTHON_VERSION} "3.6")
 
-message("DEBUG - CURRENT DIRECTORY ${CMAKE_CURRENT_LIST_DIR}")
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/FindOpenEXR.cmake DESTINATION ${SOURCE_PATH}/src/cmake/modules)
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/imageio.cpp DESTINATION ${SOURCE_PATH}/src/libOpenImageIO)
-message("DEBUG - COPIED FIND_OPEN_EXR FILE!")
-message("DEBUG - COPIED PATCHED imageio.cpp FILE!")
+# ENV AGENT_OS is an environment variable defined by Azure Pipelines
+if($ENV{AGENT_OS} STREQUAL "Linux")
+    message("DEBUG - CURRENT DIRECTORY ${CMAKE_CURRENT_LIST_DIR}")
+    file(COPY ${CMAKE_CURRENT_LIST_DIR}/FindOpenEXR.cmake DESTINATION ${SOURCE_PATH}/src/cmake/modules)
+    file(COPY ${CMAKE_CURRENT_LIST_DIR}/imageio.cpp DESTINATION ${SOURCE_PATH}/src/libOpenImageIO)
+    message("DEBUG - COPIED FIND_OPEN_EXR FILE!")
+    message("DEBUG - COPIED PATCHED imageio.cpp FILE!")
+endif()
 
 if(WIN32)
     set(HIDE_SYMBOLS ON)
